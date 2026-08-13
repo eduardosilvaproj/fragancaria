38bdcd675a274123af0d870886067e5e5587e165
Eduardo Silva <eduardosilva.proj@gmail.com>
2026-08-13 05:33:19 -0300

    chore: remove scripts de sondagem com chaves hardcoded da raiz

--- do-commit.mjs
+++
@@ -1,4 +0,0 @@
-import { execSync } from "child_process";
-execSync("git add -A", { stdio: "inherit" });
-execSync('git -c core.hooksPath=/dev/null commit -m "fix: log de erro nas tools + duplicacao + canal"', { stdio: "inherit" });
-execSync("git push origin main", { stdio: "inherit" });
