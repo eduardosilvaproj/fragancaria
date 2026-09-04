abada5e122644f697a760e80cdfad3147c307dbf
Eduardo Silva <eduardosilva.proj@gmail.com>
2026-09-04 17:31:30 -0300

    fix(analytics): dataLayer.push(arguments) para o gtag processar os comandos

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -22,9 +22,11 @@ export function initGA() {
   document.head.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
-  window.gtag = function gtag(...args: unknown[]) {
-    window.dataLayer?.push(args);
-  };
+  function gtag(..._args: unknown[]) {
+    // eslint-disable-next-line prefer-rest-params
+    window.dataLayer?.push(arguments);
+  }
+  window.gtag = gtag;
   window.gtag('js', new Date());
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_title: document.title,
